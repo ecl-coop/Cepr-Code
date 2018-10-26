e229fe6811e4128d378923a6977a673094720fa7
Zerp <grafico@ecl.com.ve>
2018-10-26 18:01:22 -0400

    include Hover and Active States

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,41 @@
-var nav = document.querySelector('.nav__toggle');
+var menubar = document.querySelector('.menubar__toggle');
+var menu = document.querySelector('.menu__toggle');
+var submenu = document.querySelector('.submenu__toggle');
+var backMenubar = document.querySelector('.backto-menubar');
+var backMenu = document.querySelector('.backto-menu');
 
 var toggleState = function (elem, one, two) {
     var elem = document.querySelector(elem);
     elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
 };
 
-nav.onclick = function (e) {
-    toggleState('#nav ul', 'closed', 'open');
+menubar.onclick = function (e) {
+    toggleState('#menubar-container', 'closed', 'open');
+    toggleState('.menubar__header', 'closed', 'open')
+    e.preventDefault();
+};
+menu.onclick = function (e) {
+    toggleState('#menubar-container', 'open', 'inside-1');
+    toggleState('.menu-container', 'closed', 'open');
+    e.preventDefault();
+};
+
+submenu.onclick = function (e) {
+    toggleState('#menubar-container', 'inside-1', 'inside-2');
+    toggleState('.menu-container', 'open', 'closed');
+    toggleState('.submenu-container', 'closed', 'open');
+    e.preventDefault();
+};
+
+backMenu.onclick = function (e) {
+    toggleState('#menubar-container', 'inside-1', 'inside-2');
+    toggleState('.menu-container', 'open', 'closed');
+    toggleState('.submenu-container', 'closed', 'open');
+    e.preventDefault();
+};
+
+backMenubar.onclick = function (e) {
+    toggleState('#menubar-container', 'open', 'inside-1');
+    toggleState('.menu-container', 'open', 'closed');
     e.preventDefault();
 };
