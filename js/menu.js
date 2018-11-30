ad2421d55050d746ff8c0831fdb551e84c391c79
Zerp <grafico@ecl.com.ve>
2018-11-30 16:24:26 -0400

    desktop update and install waypoints

--- js/menu.js
+++ js/menu.js
@@ -34,7 +34,7 @@ if (screen.width <= 768) {
         $("#accordian a").click(function () {
             var link = $(this);
             var closest_ul = link.closest("ul");
-            var parallel_active_links = closest_ul.find(".active")
+            var parallel_active_links = closest_ul.find(".active");
             var closest_li = link.closest("li");
             var link_status = closest_li.hasClass("active");
             var count = 0;
@@ -48,8 +48,8 @@ if (screen.width <= 768) {
                 closest_li.children("ul").slideDown();
                 closest_li.addClass("active");
             }
-        })
-    })
+        });
+    });
 
 } else {
     menu.onclick = function (e) {
