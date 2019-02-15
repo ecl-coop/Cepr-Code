38ead306503ddcd08f573f72b8ca8e2d1c4956bd
Zerp <grafico@ecl.com.ve>
2019-02-15 18:21:43 -0400

    report markup basic

---
+++ js/TableOfContents.js
@@ -0,0 +1,31 @@
+////Grabbed from https://codepen.io/chriscoyier/pen/EnLwb
+
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>On this page:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h3").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".all-questions").prepend(ToC);
