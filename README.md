c1995330a39d048eb176f7a15e298e17546121ba
Zerp <grafico@ecl.com.ve>
2019-04-16 17:29:57 -0400

    general index update (follow banner, icons, etc)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cepr-Code
+Cepr Frontend Development
+This is the base CSS of the components for the redesign of Cepr.net.
+All the Js functionality is only the minimum ammount to show some functionality, but should be redone by the development team.
--- js/TableOfContents.js
+++ js/TableOfContents.js
@@ -1,31 +1,31 @@
-////Grabbed from https://codepen.io/chriscoyier/pen/EnLwb
-
-var ToC =
-  "<nav role='navigation' class='table-of-contents' id='report-toc'>" +
-    "<details><summary>Contents</summary>" +
-    "<dl>";
-
-var newLine, el, title, link;
-
-$("section summary").each(function() {
-
-  el = $(this);
-  title = el.text();
-  link = "#" + el.attr("id");
-
-  newLine =
-    "<dt>" +
-      "<a href='" + link + "'>" +
-        title +
-      "</a>" +
-    "</dt>";
-
-  ToC += newLine;
-
-});
-
-ToC +=
-   "</dl></details>" +
-  "</nav>";
-
+////Grabbed from https://codepen.io/chriscoyier/pen/EnLwb
+
+var ToC =
+  "<nav role='navigation' class='table-of-contents' id='report-toc'>" +
+    "<details><summary>Contents</summary>" +
+    "<dl>";
+
+var newLine, el, title, link;
+
+$("section summary").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<dt>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</dt>";
+
+  ToC += newLine;
+});
+
+ToC +=
+
+   "</dl></details>" +
+  "</nav>";
+
 $("article").prepend(ToC);
