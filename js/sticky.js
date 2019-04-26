f64dcb16eb97d6b5c4570b73da4f254968660c6e
Zerp <grafico@ecl.com.ve>
2019-04-26 18:33:42 -0400

    menu tablet and desktop adjusments, almost done

--- js/sticky.js
+++ js/sticky.js
@@ -5,8 +5,11 @@ $(document).ready(function () {
     let $nav = $(".nav");
     let $ToC= $(".table-of-contents details");
     let $share= $(".icons-share");
-
-    $nav.waypoint(function (direction){
+    let $footnotes= $("#footnotes-desktop");
+    //let $footer= $(".block-footer");
+    let $progress = $('progress');
+    
+    $globalnav.waypoint(function (direction){
 
         if (direction == "down"){
             $globalnav.addClass("menubar-sticky");
@@ -21,7 +24,31 @@ $(document).ready(function () {
         }
 
     }, {
-        offset: "-320px"
+        offset: "-90%"
+    });
+    $globalnav.waypoint(function (direction){
+        if (direction == "up"){
+            $globalnav.removeClass("menubar-sticky");
+            $nav.removeClass("nav-sticky");
+            $ToC.removeClass("toc-sticky");
+            $share.removeClass("share-sticky");
+        }
+    
+    });
+    var waypoints = 
+    $footnotes.waypoint({
+        handler: function(direction){
+            if(direction == "down"){
+                $ToC.removeClass("toc-sticky");
+                $share.removeClass("share-sticky");
+                $progress.addClass("is-hidden");
+            } else {
+                $ToC.addClass("toc-sticky");
+                $share.addClass("share-sticky");
+                $progress.removeClass("is-hidden");
+            }
+        }
+    }, {
+        offset: '40%'
     });
 });
-
