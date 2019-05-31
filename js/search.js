80a85b717a5c203aef8033ce6b747c6df2611bbc
Zerp <grafico@ecl.com.ve>
2019-05-30 21:36:10 -0400

    finish search page

--- js/search.js
+++ js/search.js
@@ -1,5 +1,10 @@
+//I had to hack an extra "-dkt" button class for it to work, because the query selector only selected the mobile object but not the desktop one. 
+//Is a lot less than ideal. 
+
 var searchBtn = document.querySelector('.menubar__search');
+var searchBtnDkt = document.querySelector('.menubar__search-dkt');
 var searchPage = document.querySelector('.search-field');
+var sortBtn = document.querySelector('.btn-sort');
 
 var toggleState = function (elem, one, two) {
     var elem = document.querySelector(elem);
@@ -13,5 +18,14 @@ var setStatesTablet = function (elem, state) {
         toggleState('.search-toggle', 'closed', 'open');
         toggleState('.menubar__header', 'closed', 'open');
         toggleState('.search-container', 'closed', 'open');
+        e.preventDefault();
+    };
+    searchBtnDkt.onclick = function (e) {
+        toggleState('.search-toggle', 'closed', 'open');
+        toggleState('.search-container', 'closed', 'open');
+        e.preventDefault();
+    };
+    sortBtn.onclick = function (e) {
+        toggleState('.btn-sort', 'down', 'up');
         e.preventDefault();
     };
